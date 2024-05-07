e310450cf05f98b4ced45f1fc6325cbe81d145fa
Kori Miyohashi <miyohashikori457@gmail.com>
2024-05-07 17:22:53 +0800

    feat: template for thread commit

--- src/thread_pool/thread_pool.h
+++ src/thread_pool/thread_pool.h
@@ -9,6 +9,8 @@
 #include <queue>
 #include <thread>
 #include <vector>
+#include <future>
+#include <functional>
 
 namespace thread_pool {
     class ThreadPool {
@@ -21,15 +23,49 @@ namespace thread_pool {
         std::queue<Task>         _tasks;
         std::vector<std::thread> _pool;
     public:
+        /// @brief Get the instance of the ThreadPool
+        /// @return The instance of the ThreadPool 
         static ThreadPool& instance();
 
+        /// @brief Initialize the ThreadPool
+        /// @param num The maximum number of threads in the pool 
         void init(int num = std::thread::hardware_concurrency());
 
+        /// @brief Add a task to the ThreadPool
+        /// @param task The task to be added
+        /// @note The task should be a no return and no parameter function 
         void add_task(Task&& task);
 
+        /// @brief Get the number of idle threads in the ThreadPool
+        /// @return The number of idle threads
         int idle_thread_num();
 
         ~ThreadPool();
+
+        /// @brief Submit a task to the ThreadPool
+        /// @tparam F The type of the task
+        /// @tparam ...Args The type of the arguments
+        /// @param f The task to be submitted 
+        /// @param ...args The arguments of the task
+        /// @return The future of the task 
+        template<typename F, typename... Args>
+        auto submit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+            auto task =
+                std::make_shared<std::packaged_task<decltype(f(args...))()>>
+                (std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+            auto res = task->get_future();
+            add_task([task]() { (*task)(); });
+            return res;
+
+            // auto task =
+            //     std::make_unique<std::packaged_task<decltype(f(args...))()>>
+            //     (std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+            // auto res = task->get_future();
+            // add_task([task=std::move(task)]() { (*task)(); });
+            // return res;
+        }
     private:
         ThreadPool() :_is_stop(false) {};
 
